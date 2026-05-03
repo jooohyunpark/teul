cd5050165ab489e787b7b1420803ba62d0dae377
Joohyun Park <jhp527@nyu.edu>
2026-05-03 15:27:56 -0400

    add padding right to secure visible space in code block

--- components/site/code-block.tsx
+++ components/site/code-block.tsx
@@ -23,7 +23,7 @@ export async function CodeBlock({
         <CodeBlockCopyButton text={code} />
       </div>
       <div
-        className="rounded border bg-card p-4 text-sm leading-relaxed [&_code]:font-mono [&_pre]:overflow-x-auto [&_pre]:bg-transparent!"
+        className="rounded border bg-card p-4 pr-12 text-sm leading-relaxed [&_code]:font-mono [&_pre]:overflow-x-auto [&_pre]:bg-transparent!"
         dangerouslySetInnerHTML={{ __html: html }}
       />
     </div>
